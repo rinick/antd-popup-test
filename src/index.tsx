6dc47a7459732b01212837b8670a9a3668811480
rinick <rinick@gmail.com>
2020-12-19 13:04:25 -0800

    map rect to/from window

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,28 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import ReactDOM from 'react-dom';
 import {Select, Switch, Slider, TimePicker, Popover, Button, Tooltip, Popconfirm, Modal, notification} from 'antd';
 import NewWindow from 'rc-new-window';
+import {mapElementToScreenRect, mapWindowToElement} from 'rc-new-window/lib/ScreenPosition';
 
 const {Option} = Select;
 
 const App = () => {
   const [popup, setPopup] = useState(false);
   const [model, setModel] = useState(false);
+  const [win, setWin] = useState<Window>(null);
+  const [style, setStyle] = useState({left: 30, top: 60, width: 400, height: 400});
+
+  const ref = useRef();
+  const initPosition = () => {
+    return mapElementToScreenRect(ref.current, style);
+  };
 
   function onChange(checked: boolean = false) {
+    if (!checked && win) {
+      setStyle(mapWindowToElement(ref.current, win));
+    }
     setPopup(checked);
+    setWin(null);
   }
 
   function showModal() {
@@ -27,35 +39,35 @@ const App = () => {
   };
 
 
-  let content = (<div className='main'>
+  let content = (<div className='main' style={style}>
     <Select defaultValue="1" style={{width: 120}}>
       <Option value="1">Select</Option>
       <Option value="2">Select</Option>
       <Option value="3">Select</Option>
     </Select>
-    {" "}
+
     <Popover content={<div>
       <p>Content</p>
       <p>Content</p>
     </div>} title="Title">
       <Button type="primary">Popover</Button>
     </Popover>
-    {" "}
+
     <TimePicker/>
-    {" "}
+
     <Tooltip title="prompt text">
       <Button type="primary">Tooltip</Button>
     </Tooltip>
-    {" "}
+
     <Button type="primary" onClick={openNotification}>
       notification
     </Button>
-    {" "}
+
     <Popconfirm title={'Popconfirm'}>
       <Button>Popconfirm</Button>
     </Popconfirm>
     <hr/>
-    {" "}
+
     <Button type="primary" onClick={showModal}>
       Open Modal
     </Button>
@@ -64,20 +76,20 @@ const App = () => {
       <p>Some contents...</p>
       <p>Some contents...</p>
     </Modal>
-    {" "}
+
     <Slider range={true} defaultValue={[20, 50]}/>
   </div>);
 
   if (popup) {
     content = (
-      <NewWindow onClose={onChange}>
+      <NewWindow onClose={onChange} onOpen={setWin} initPopupInnerRect={initPosition}>
         {content}
       </NewWindow>
     );
   }
 
   return (
-    <div className='main'>
+    <div ref={ref}>
       <span>
          Popup: <Switch checked={popup} onChange={onChange}/>
       </span>
